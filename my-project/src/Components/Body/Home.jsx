91abc71ef5802e94fa58fa6a88352d98b937a8f7
Abdelhakim Outas <outasabdelhakim@gmail.com>
2024-08-05 17:09:17 +0100

    add some modificatioon

--- my-project/src/Components/Body/Home.jsx
+++ my-project/src/Components/Body/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link,  } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
@@ -7,7 +8,7 @@ function Home() {
       <div className='textHome'>
         <h1>أزياء النجمات</h1>
         <h3>كوني نجمة من نجومنا</h3>
-        <a href="#" class="cta-button">تسوق الآن</a>
+        <button><Link to='shop'>تسوق الآن</Link></button>
       </div>
       
     </div>
